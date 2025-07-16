ad72b35daec57d76f9050590be317343a348dc4f
YELDAMUMESH <gliscor68@gmail.com>
2025-07-16 15:56:16 +0530

    Added prog on pyjokes

---
+++ Chapter_1/module.py
@@ -0,0 +1,14 @@
+ #devloper : Umesh
+ #date : 26/04/2022
+
+
+import pyjokes
+
+print("Printing jokes............")
+music1=pyjokes.get_joke() #function call
+music2=pyjokes.get_joke()
+print("Joke_1")
+print(music1)
+print("Joke_2")
+print(music2)
+
