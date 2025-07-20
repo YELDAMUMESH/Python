0ad254b31a881404b139b7f65f1cc44784e1fdb3
YELDAMUMESH <gliscor68@gmail.com>
2025-07-20 21:24:46 +0530

    Added prog to add elements in list using append function

---
+++ Chapter_4/fruits.py
@@ -0,0 +1,13 @@
+fruit_basket=[]
+print("Enter fruit name: ")
+fruit_basket.append(input())
+print("Enter fruit name: ")
+fruit_basket.append(input())
+print("Enter fruit name: ")
+fruit_basket.append(input())
+print("Enter fruit name: ")
+fruit_basket.append(input())
+print("Enter fruit name: ")
+fruit_basket.append(input())
+
+print(fruit_basket)
