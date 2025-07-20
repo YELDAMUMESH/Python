08ab95906fbae56722967e8456364dbf9455c222
YELDAMUMESH <gliscor68@gmail.com>
2025-07-20 21:25:59 +0530

    Added prog on sort function

---
+++ Chapter_4/Marks.py
@@ -0,0 +1,11 @@
+marks=[]
+marks.append(int(input("Enter marks: ")))
+marks.append(int(input("Enter marks: ")))
+marks.append(int(input("Enter marks: ")))
+marks.append(int(input("Enter marks: ")))
+marks.append(int(input("Enter marks: ")))
+
+
+print("Before sorting: ",marks)
+marks.sort()
+print("After sorting: ",marks)
