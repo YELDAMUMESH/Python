50d38389bce2541e022405c3979be556c65120e1
YELDAMUMESH <gliscor68@gmail.com>
2025-07-21 21:13:34 +0530

    Added prog on friends data

---
+++ Chapter_5/4_friends.py
@@ -0,0 +1,27 @@
+friends_collection={}
+
+name=input("Enter your name: ")
+lang=input("Enter your fav language: ")
+friends_collection.update({name:lang})
+
+
+
+name=input("Enter your name: ")
+lang=input("Enter your fav language: ")
+friends_collection.update({name:lang})
+
+
+name=input("Enter your name: ")
+lang=input("Enter your fav language: ")
+friends_collection.update({name:lang})
+
+
+name=input("Enter your name: ")
+lang=input("Enter your fav language: ")
+friends_collection.update({name:lang})
+
+print(friends_collection)
+
+
+#Notes : 1. What if friends have common names - lang will be updated every time 
+#        2. What if friends have common fav lang - Nothing will happen.
