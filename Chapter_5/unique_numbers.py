7497fd14b31a4a49bb70813db9bdfc188ac88245
YELDAMUMESH <gliscor68@gmail.com>
2025-07-21 21:18:24 +0530

    Added prog to find if set stores duplicate data

---
+++ Chapter_5/unique_numbers.py
@@ -0,0 +1,28 @@
+
+collection=set()
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+num=int(input("Enter any number : "))
+collection.add(num)
+
+print(collection)
