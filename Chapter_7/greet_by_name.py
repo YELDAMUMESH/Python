c56e29c43eb5bdabaef176c469dffde9e4bd29b1
YELDAMUMESH <gliscor68@gmail.com>
2025-07-25 22:55:54 +0530

    Added prog to greet the person

---
+++ Chapter_7/greet_by_name.py
@@ -0,0 +1,9 @@
+#Greet the person whose name starts with "A"
+
+names_collection=["Umesh","Akhil","Arjun","Abhishek"]
+
+for name in names_collection:
+    if name.startswith("A"):
+        print(f"Hello,{name}")
+        
+
